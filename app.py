8f415b9a720ff5debeddddbb505d3bb7bb3571e2
Manish Kumar <55057062+man1sh1409@users.noreply.github.com>
2023-04-15 21:54:24 +0530

    Add files via upload
    backend file

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask,render_template,jsonify
+
+from flask import request as req 
+
+import pickle
+
+app=Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    return render_template('index.html')
+
+
+
+
+@app.route('/detect',methods=['GET','POST'])
+def detection():
+    return render_template('detection.html')
+    
+@app.route('/detect/predict',methods=['GET','POST'])
+def predict():
+    
+    userid=""
+    title=""
+    if req.method=='POST':
+        userid=req.form['userid']
+        title=req.form['tweets']
+        # pipeline=pickle.load(open('model.pkl','rb'))
+        print(userid)
+    res=1
+    msg=""
+    if(res):msg="Fake"
+    else:msg="Real"
+    # res=pipeline.predict([title]) 
+    
+      
+    return render_template('detection.html',result=res,msg=msg)
+    
+    
+    
+    
+if __name__=='__main__':
+    debug=False
+    print("app is running")
+    app.run()
